12e7a8b3564fc3a4819bf51818155f7a5c0a59f5
mrfauzii <reishifauzi24@gmail.com>
2026-04-13 15:53:43 +0700

    Add message caching functionality with user creation and command handling

--- web/database/migrations/0001_01_01_000001_create_cache_table.php
+++ web/database/migrations/0001_01_01_000001_create_cache_table.php
@@ -1,21 +1,35 @@
 <?php
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
-        Schema::create('message_cache', function (Blueprint $table) {
-            $table->id();
-            $table->string('sender_number');
-            $table->text('latest_message');
-            $table->timestamps(); // opsional, tapi disarankan
+        Schema::create('cache', function (Blueprint $table) {
+            $table->string('key')->primary();
+            $table->mediumText('value');
+            $table->integer('expiration')->index();
+        });
+
+        Schema::create('cache_locks', function (Blueprint $table) {
+            $table->string('key')->primary();
+            $table->string('owner');
+            $table->integer('expiration')->index();
         });
     }
 
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
-        Schema::dropIfExists('message_cache');
+        Schema::dropIfExists('cache');
+        Schema::dropIfExists('cache_locks');
     }
 };
