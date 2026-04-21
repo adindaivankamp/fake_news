0f1fcdbb0469e9e21da32c04d0db5a7fbab73cbc
Purnama Ridzky Nugraha <purnamanugraha492@gmail.com>
2026-04-22 06:00:05 +0700

    feat: Update cari_link function to use requests for API calls and enhance error handling

--- ai-api/services/text_stage2/search_service.py
+++ ai-api/services/text_stage2/search_service.py
@@ -1,6 +1,6 @@
 import urllib.parse
 from config.trusted_news_websites import trusted_news_websites
-from ddgs import DDGS
+import requests
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120 Safari/537.36"}
 
 
@@ -18,23 +18,36 @@ def is_trusted(url):
     return any(trusted in domain for trusted in trusted_news_websites)
 
 def cari_link(judul):
-    with DDGS() as ddgs:
-        results = list(ddgs.text(judul, max_results=5))  
+    try:
+        res = requests.get(
+            "http://localhost:8080/search",
+            params={
+                "q": judul,
+                "language": "id",
+                "safesearch": 1,
+                "categories": "news",
+                "format": "json"
+            },
+            timeout=10,
+            headers=headers
+        )
 
-    for r in results:
-        href = r.get("href")
-        if not href:
-            continue
+        data = res.json()
+        results = data.get("results", [])
 
-        if is_trusted(href):
-            print(f"Found trusted link: {href}")
-            return href
+        for r in results:
+            href = r.get("url")
+            if not href:
+                continue
 
-    return None
+            if is_trusted(href):
+                return href
 
-def clean_ddg(url):
-    if url and "uddg=" in url:
-        url = url.split("uddg=")[1].split("&")[0]
-        url = urllib.parse.unquote(url)
-    return url
+        return None
+
+    except Exception as e:
+        print(f"Error cari_link: {e}")
+        return None
+
+
 
